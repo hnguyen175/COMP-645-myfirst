003c40757ac9c2f5078b399efed6d5ca1ea129a2
HN <dmyaho1@hotmail.com>
2026-08-26 15:20:10 -0700

    clean up

--- www/script/page.js
+++
@@ -1,9 +0,0 @@
-function onShowIPClick(btnId) {
-    const ipButton = document.querySelector(btnId);
-    fetch("https://api.ipify.org?format=json")
-    .then(response => response.json())
-    .then(data => {
-        ipButton.value = `${data.ip}`;
-    })
-    .catch(error => console.error('Error fetching IP address:', error));
-};
